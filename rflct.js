9c965b8859fc1cad475ab245b5ae4c7b74491170
Eldrin Banares <55264397+ebanares@users.noreply.github.com>
2020-03-25 17:51:42 -0400

    Add files via upload

---
+++ rflct.js
@@ -0,0 +1,10 @@
+console.log("hi i'm alive")
+
+
+window.onload = function(){
+	// var num = Math.random()*100; //by multiply by 100, it'll give this number a "ceiling"; 
+	// num = Math.floor(num); //takes whatever number you have and rounds it DOWN; cuts off the decimal;
+	
+	// $(".overlapContainer").animate("transform","translate("+num+"px,"+num+"px);");
+	$(".weekOne, .weekTwo, .weekThree, .weekFour, .weekFive, .weekSix, .weekSeven, .weekEight, .weekNine, .weekTen, .weekEleven, .weekTwelve").draggable(); //this is just making it able to move the paragraph;
+	};
